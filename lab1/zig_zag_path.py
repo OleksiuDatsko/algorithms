707038a83e1a2dbca9be0ac39aaf89446b41bcbf
OleksiuDatsko <datskooleksiu@gmail.com>
2023-09-20 20:03:22 +0300

    removed unnecessary prints

--- lab1/zig_zag_path.py
+++ lab1/zig_zag_path.py
@@ -1,11 +1,12 @@
 
-def generate_matrix(n: int, m: int) -> list[list[int]]:
+def generate_matrix(n: int, m: int, print_matrix: bool = False) -> list[list[int]]:
     result = []
     sub_result = []
     for i in range(n):
         for j in range(m):
             sub_result.append((j + i) % (n + m - 1))
-        print(sub_result)
+        if print_matrix:
+            print(sub_result)
         result.append(sub_result)
         sub_result = []
     return result
@@ -22,14 +23,12 @@ def zig_zag_path(matrix: list[list[any]]) -> list[any]:
     diagonals = n + m - 1
     
     for diagonal in range(diagonals):
-        print(f"== {diagonal} ==")
         if diagonal % 2 == 0:
             if diagonal < n:
                 i, j = diagonal, 0
             if diagonal >= n:
                 i, j = n - 1, diagonal - n + 1
             while i >= 0 and j <= m - 1:
-                print(f"{matrix[i][j]} ({i}, {j})")
                 result.append(matrix[i][j])
                 i -= 1
                 j += 1
@@ -38,13 +37,8 @@ def zig_zag_path(matrix: list[list[any]]) -> list[any]:
                 i, j = 0, diagonal
             if diagonal >= m:
                 i, j = diagonal - m + 1, m - 1
-            print(i + j == diagonal, diagonal, n, m) 
             while j >= 0 and i <= n - 1:
-                print(f"{matrix[i][j]} ({i}, {j})")
                 result.append(matrix[i][j])
                 j -= 1
                 i += 1
     return result
-
-if __name__ == "__main__":
-    print(zig_zag_path(generate_matrix(3, 5)))
