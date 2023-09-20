6dd2682095166bfb75b8dd52c21f799222a2ca09
OleksiuDatsko <datskooleksiu@gmail.com>
2023-09-20 18:50:03 +0300

    added first lab

---
+++ lab1/tests.py
@@ -0,0 +1,49 @@
+import unittest
+
+from zig_zag_path import generate_matrix, zig_zag_path
+
+
+def get_zig_zag_result(n, m):
+    return zig_zag_path(generate_matrix(n, m))
+
+
+class TestZigZagPath(unittest.TestCase):
+
+    def test_zig_zag_path_n5_m5(self):
+        zig_zag_result = get_zig_zag_result(n=5, m=5)
+        self.assertEqual(
+            zig_zag_result,
+            [0, 1, 1, 2, 2, 2, 3, 3, 3, 3, 4, 4, 4, 4, 4, 5, 5, 5, 5, 6, 6, 6, 7, 7, 8],
+        )
+
+    def test_zig_zag_path_n4_m2(self):
+        zig_zag_result = get_zig_zag_result(n=4, m=2)
+        self.assertEqual(
+            zig_zag_result,
+            [0, 1, 1, 2, 2, 3, 3, 4],
+        )
+
+    def test_zig_zag_path_n1_m6(self):
+        zig_zag_result = get_zig_zag_result(n=1, m=6)
+        self.assertEqual(
+            zig_zag_result,
+            [0, 1, 2, 3, 4, 5],
+        )
+
+    def test_zig_zag_path_n1_m1(self):
+        zig_zag_result = get_zig_zag_result(n=1, m=1)
+        self.assertEqual(
+            zig_zag_result,
+            [0],
+        )
+
+    def test_zig_zag_path_n0_m0(self):
+        zig_zag_result = get_zig_zag_result(n=0, m=0)
+        self.assertEqual(
+            zig_zag_result,
+            [],
+        )
+
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ lab1/zig_zag_path.py
@@ -0,0 +1,51 @@
+
+def generate_matrix(n: int, m: int) -> list[list[int]]:
+    result = []
+    sub_result = []
+    for i in range(n):
+        for j in range(m):
+            sub_result.append((j + i) % (n + m - 1))
+        print(sub_result)
+        result.append(sub_result)
+        sub_result = []
+    return result
+
+
+def zig_zag_path(matrix: list[list[any]]) -> list[any]:
+    if not matrix:
+        return []
+
+    n = len(matrix)
+    m = len(matrix[0])
+    result = []
+
+    diagonals = n + m - 1
+    
+    for diagonal in range(diagonals):
+        print(f"== {diagonal} ==")
+        if diagonal % 2 != 0:
+            if diagonal < n:
+                i, j = diagonal, 0
+            if diagonal >= n:
+                i, j = n - 1, diagonal - n + 1
+            print(i + j == diagonal, diagonal, n, m) 
+            while i >= 0 and j <= m - 1:
+                print(f"{matrix[i][j]} ({i}, {j})")
+                result.append(matrix[i][j])
+                i -= 1
+                j += 1
+        if diagonal % 2 != 1:
+            if diagonal < m:
+                i, j = 0, diagonal
+            if diagonal >= m:
+                i, j = diagonal - m + 1, m - 1
+            print(i + j == diagonal, diagonal, n, m) 
+            while j >= 0 and i <= n - 1:
+                print(f"{matrix[i][j]} ({i}, {j})")
+                result.append(matrix[i][j])
+                j -= 1
+                i += 1
+    return result
+
+if __name__ == "__main__":
+    print(zig_zag_path(generate_matrix(3, 5)))
