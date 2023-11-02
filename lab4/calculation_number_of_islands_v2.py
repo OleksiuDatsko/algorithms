33103f840568f23f22fbc9e014e8b7569fb78ecc
OleksiuDatsko <datskooleksiu@gmail.com>
2023-11-02 11:12:31 +0200

    read from file v2

--- lab4/calculation_number_of_islands_v2.py
+++ lab4/calculation_number_of_islands_v2.py
@@ -1,36 +1,45 @@
 import ast
 
 neighborhood_modificators = [
-    (1, -1),
+    (1, -3),
     (1, 0),
-    (1, 1),
-    (0, -1),
-    (0, 1),
-    (-1, -1),
+    (1, 3),
+    (0, -3),
+    (0, 3),
+    (-1, -3),
     (-1, 0),
-    (-1, 1),
+    (-1, 3),
 ]
 
 
-def cheack_colisions(i: int, j: int, matrix: list[list]) -> bool:
-    return 0 <= i < len(matrix) and 0 <= j < len(matrix[0])
+def cheack_colisions(i: int, j: int, rows: int, cols: int) -> bool:
+    return 0 <= i < rows and 0 <= j < cols
 
 
 def convert_file_to_adjacency_list(input_file):
+    adjacency_list = {}
+
     with open(input_file, "r") as f:
         lines = f.readlines()
-        matrix = [list(ast.literal_eval(row)) for row in lines]
 
-    adjacency_list = {}
-    for i in range(len(matrix)):
-        for j in range(len(matrix[i])):
-            if matrix[i][j] == 1:
+    rows = len(lines)
+    cols = len(lines[0]) - 1
+    print(rows, cols)
+    for i in range(rows):
+        for j in range(cols - 1):
+            if lines[i][j] == "1":
                 neighbors = []
                 for i_m, j_m in neighborhood_modificators:
                     n_i, n_j = i + i_m, j + j_m
-                    if cheack_colisions(n_i, n_j, matrix) and matrix[n_i][n_j] == 1:
-                        neighbors.append((n_i, n_j))
-                adjacency_list[(i, j)] = neighbors
+                    if (
+                        cheack_colisions(n_i, n_j, rows, cols)
+                        and lines[n_i][n_j] == "1"
+                    ):
+                        neighbors.append((n_i, n_j // 3))
+                adjacency_list[(i, j // 3)] = neighbors
+
+    for key, value in adjacency_list.items():
+        print(key, value)
 
     return adjacency_list
 
@@ -62,4 +71,3 @@ def number_of_islands() -> int:
 
 if __name__ == "__main__":
     print("result:", number_of_islands())
-
